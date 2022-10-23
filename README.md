da7db7f81b1e4a01e2839db4ecea0036f178cf5e
Pierric Gimmig <pierric.gimmig@gmail.com>
2022-10-22 17:08:13 -0700

    Add project files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# minimgui
-Minimal ImGui setup using Conan and CMake
+## MinImGui
+
+MinImgui is a bare-bones project using Conan and CMake to build a cross-platform ImGui demo.
+
+## Build and run:
+
+```bash
+mkdir build
+cd build
+conan install .. -s build_type=Release
+conan install .. -s build_type=Debug
+cmake ..
+cmake --build . --config Release
+./Release/minimgui(.exe)
+```
---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include "imgui.h"
+#include "bindings/imgui_impl_glfw.h"
+#include "bindings/imgui_impl_opengl3.h"
+
+#include <stdio.h>
+#include <string>
+
+#include <GL/glew.h> // Initialize with glewInit()
+
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
+void UserRender();
+
+static void glfw_error_callback(int error, const char *description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+std::string GetGlslVersion()
+{
+#if __APPLE__
+	// GL 3.2 + GLSL 150
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);		   // Required on Mac
+	return "#version 150";
+#else
+	// GL 3.0 + GLSL 130
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	return "#version 130";
+#endif
+}
+
+void SetGlViewportForWindow(GLFWwindow *window)
+{
+	int window_width = 0;
+	int window_height = 0;
+	glfwGetFramebufferSize(window, &window_width, &window_height);
+	glViewport(0, 0, window_width, window_height);
+}
+
+int main(int, char **)
+{
+	// Setup window
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
+		return 1;
+
+	std::string glsl_version = GetGlslVersion();
+
+	// Create window with graphics context
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui - Conan", NULL, NULL);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to create GLFW window.\n");
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	if (glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Failed to initialize OpenGL loader.\n");
+		return 1;
+	}
+
+	SetGlViewportForWindow(window);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version.c_str());
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// feed inputs to dear imgui, start new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		UserRender();
+
+		// Render dear imgui into screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		SetGlViewportForWindow(window);
+		glfwSwapBuffers(window);
+	}
+
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+void UserRender()
+{
+	static bool demo = true;
+	ImGui::ShowDemoWindow(&demo);
+}
