3e17117066db2e7bf7ad383e37d35b9fefd94357
Pierric Gimmig <pierric.gimmig@gmail.com>
2024-11-17 21:08:08 -0800

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf build
 mkdir build
 cd build
-conan install ..
-cmake ..
-cmake --build .
+conan install .. --build=missing
+cmake .. -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build . 
 ./bin/minimgui
